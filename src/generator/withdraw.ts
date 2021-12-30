2107f4dfb013f42035fb15c2c48b660fa27712f4
duanyytop <duanyytop@gmail.com>
2021-12-30 15:30:46 +0800

    feat: Add withdraw and update start command

---
+++ src/generator/withdraw.ts
@@ -0,0 +1,11 @@
+import { withdrawCotaNFT } from '../rpc/cota'
+
+const run = async () => {
+  const cotaOutPoint: CKBComponents.OutPoint = {
+    txHash: '0x49fdb28bb57c51817c30c7dc4e3b2eccf8a93b20eecf4b41abfa64ed0cb52b30',
+    index: '0x2',
+  }
+  await withdrawCotaNFT(cotaOutPoint)
+}
+
+run()
